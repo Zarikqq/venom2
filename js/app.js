2ef720e82bf88d4fda539a99e7d91b40b6b8ec47
Zarikqq <85572842+Zarikqq@users.noreply.github.com>
2021-06-11 13:26:38 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const menuButton = document.querySelector('.menu_button');
+const menu = document.querySelector('.nav__menu')
+const menuCloseButton =document.querySelector('.menu_close')
+
+menuButton.addEventListener('click', ()=> {
+    menu.classList.add('is_active');
+    menuCloseButton.classList.add('is_active');
+})
+menuCloseButton.addEventListener('click', ()=> {
+    menu.classList.remove('is_active');
+    menuCloseButton.classList.remove('is_active');
+})
